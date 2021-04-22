34a1993569a01d5608f1b6bd41fc85aaa9a465cf
Ivan Stantic <stantic.ivan@gmail.com>
2021-04-22 16:17:06 +0200

    Added header and footer

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<h1>Index Page</h1>
+<?php get_header(); ?>
+
+<!-- Main Content -->
+<main>
+
+    <?php if (have_posts()) : ?>
+
+        <!-- The Loop -->
+        <?php while (have_posts()) : the_post(); ?>
+            <article>
+                <h2><?php the_title(); ?></h2>
+            </article>
+        <?php endwhile; ?>
+
+    <?php else : ?>
+        <p>Sorry, no posts matched your criteria.</p>
+    <?php endif; ?>
+
+</main>
+
+<?php get_footer(); ?>
