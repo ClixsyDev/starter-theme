5154d262fb03f0f3bef5167e9e4669232a05591b
Ivan Stantic <stantic.ivan@gmail.com>
2021-04-23 19:01:34 +0200

    Added tailwind css

--- _assets/webpack.mix.js
+++ _assets/webpack.mix.js
@@ -1,4 +1,6 @@
 let mix = require('laravel-mix');
 
 mix.js('src/js/script.js', 'public/js')
-   .css('src/css/style.css', 'public/css');
+   .css('src/css/style.css', 'public/css', [
+        require("tailwindcss"),
+   ]);
