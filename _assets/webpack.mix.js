81ab48f377ed49d3691ad8fcdb7589cc91cbd58f
Ivan Stantic <stantic.ivan@gmail.com>
2021-04-25 20:12:06 +0200

    Added postcss processing and stylelinting

--- _assets/webpack.mix.js
+++ _assets/webpack.mix.js
@@ -1,18 +1,19 @@
-let mix = require('laravel-mix');
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
+const mix = require('laravel-mix');
 
 mix.webpackConfig({
-        module: {
-            rules: [
-                {
-                    enforce: 'pre',
-                    test: /\.(js)$/,
-                    loader: 'eslint-loader',
-                    exclude: /node_modules/
-                }
-            ]
-        }
-    })
-   .js('src/js/script.js', 'public/js')
-   .css('src/css/style.css', 'public/css', [
-        require("tailwindcss"),
-   ]);
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+});
+
+mix.js('src/js/script.js', 'public/js')
+  .css('src/css/style.css', 'public/css');
