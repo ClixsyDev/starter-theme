dd39e854e561d397a87a7003ae69b179b7df6402
Ivan Stantic <stantic.ivan@gmail.com>
2021-04-26 20:55:09 +0200

    Updated the image optimization workflow

--- _assets/webpack.mix.js
+++ _assets/webpack.mix.js
@@ -2,6 +2,7 @@
 /* eslint-disable global-require */
 const mix = require('laravel-mix');
 const StylelintPlugin = require('stylelint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 
 mix.webpackConfig({
@@ -9,6 +10,11 @@ mix.webpackConfig({
     new StylelintPlugin({
       configFile: './.stylelintrc.json',
       files: './src/css/*.css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: './src/images', to: './public/images' },
+      ],
     }),
     new ImageminPlugin({
       disable: !mix.production,
@@ -23,17 +29,11 @@ mix.webpackConfig({
         loader: 'eslint-loader',
         exclude: /node_modules/,
       },
-      {
-        test: /\.(jpe?g|png|gif)$/,
-        loader: 'url-loader',
-        options: {
-          // Images larger than 10 KB won’t be inlined
-          limit: 10 * 1024,
-        },
-      },
     ],
   },
 });
 
 mix.js('src/js/script.js', 'public/js')
-  .css('src/css/style.css', 'public/css');
+  .css('src/css/style.css', 'public/css').options({
+    processCssUrls: false,
+  });
