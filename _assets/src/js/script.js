57608e70fdc3e334d657b94f55d76b76b47ce885
Ivan Stantic <stantic.ivan@gmail.com>
2021-05-03 16:22:15 +0200

    Implemented lazy loaded glide js slider

--- _assets/src/js/script.js
+++ _assets/src/js/script.js
@@ -1,4 +1,15 @@
 import lozad from 'lozad';
+// import Glide from '@glidejs/glide';
 
 const observer = lozad();
 observer.observe();
+
+// new Glide('.glide', { autoplay: 2000, dragThreshold: 1 }).mount();
+
+const mySliderObserver = lozad('.glide', {
+  load: async (el) => {
+    const { default: Glide } = await import('@glidejs/glide');
+    new Glide(el, { autoplay: 2000, dragThreshold: 1 }).mount();
+  },
+});
+mySliderObserver.observe();
